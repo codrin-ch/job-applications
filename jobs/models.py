e1558fefcd8da68edf6b58dbfafbd4affa8bd2a3
Codrin-Mihai Chira <122961778+codrin-ch@users.noreply.github.com>
2025-12-03 15:58:44 +0200

    Add source field for job application (#2)

--- jobs/models.py
+++ jobs/models.py
@@ -8,6 +8,11 @@ class JobApplication(models.Model):
         ('HR Interview', 'HR Interview'),
         ('Offer', 'Offer'),
     ]
+    SOURCE_CHOICES = [
+        ('LinkedIn', 'LinkedIn'),
+        ('Careers Website', 'Careers Website'),
+        ('Other', 'Other'),
+    ]
     job_title = models.CharField(max_length=100)
     salary = models.CharField(max_length=100, default='', blank=True)
     company_name = models.CharField(max_length=100)
@@ -15,6 +20,7 @@ class JobApplication(models.Model):
     job_description = models.TextField()
     resume_version = models.CharField(max_length=100)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Applied')
+    source = models.CharField(max_length=20, choices=SOURCE_CHOICES, default='Careers Website')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
