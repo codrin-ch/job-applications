4451b960fc6ddff4c4fd23880074d4ff1654293e
codrin-ch <chiracodrinmihai@gmail.com>
2025-12-17 17:15:58 +0200

    Remove unused files

--- data-analyzer/agent/workflows/workflow.go
+++
@@ -1,8 +0,0 @@
-package workflows
-
-import "context"
-
-// Workflow is the interface that all workflows must implement
-type Workflow[T any] interface {
-	Execute(ctx context.Context) (T, error)
-}
